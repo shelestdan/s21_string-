5ceb28d14a7115112f2887c23e2f7d3929874a3a
marnitic <daniil.shelesteev@yandex.ru>
2022-12-14 02:38:08 +0300

    add func

--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -4,32 +4,39 @@
 // #include <string.h>
 #include <unistd.h>
 
-#define CHAR_PATTERN 1000
+#define CHAR_PATTERN 4000
 
 typedef long unsigned s21_size_t;
 
 typedef struct info {
-  va_list args;
-  int width;
-  int precision;
-  int hex_size;
-  int zero_padding;
-  int point;
-  int dash;
-  int total_length;
-  int sign;
-  int is_zero;
-  int space;
-  int hash;
-  float numb_fl;
-  int j_save_format;
-  int flag_f_float;
-  int flag_o;
-  int flag_e;
-  char full_buf[CHAR_PATTERN];
+    va_list args;
+    int width;
+    int precision;
+    int hex_size;
+    int zero_padding;
+    int point;
+    int dash;
+    int total_length;
+    int sign;
+    int is_zero;
+    int space;
+    int hash;
+    float numb_fl;
+    int j_save_format;
+    int flag_f_float;
+    int flag_o;
+    int flag_e;
+    int flag_u;
+    int flag_h;
+    int flag_l;
+    int flag_s;
+    int flag_dash;
+    int flag_zero;
+    char full_buf[CHAR_PATTERN];
 } s_info;
 
 char *itoa(int value, char *result, int base);
+char *itoa_u(unsigned int value, char *result, int base);
 int s21_sprintf(char *str, const char *format, ...);
 void wparg_help(s_info *ints);
 char *s_strncat(char *dest, const char *src, s21_size_t n);
@@ -45,9 +52,9 @@ void print_hex(s_info *ints);
 void print_hexup(s_info *ints);
 void print_per(s_info *ints);
 void print_float(s_info *ints);
-int check_format(const char *format, int temp);
-int check_format_letter(const char *format, int temp);
-int check_format_letter_2(const char *format, int temp);
+int check_format(const char *format, int temp, s_info *ints);
+int check_format_letter(const char *format, int temp, s_info *ints);
+int check_format_letter_2(const char *format, int temp, s_info *ints);
 int check_percision(s_info *ints, const char *format, int temp);
 int check_point(s_info *ints, const char *format, int temp);
 int check_wp(s_info *ints, const char *format, int temp);
@@ -56,9 +63,9 @@ int n_len(int n);
 void s21_sprintf_g(s_info *ints);
 
 void flag_exp(s_info *ints);
-void ftoa(float n, char *res, s_info *ints);
+void ftoa(long double n, char *res, s_info *ints);
 void reverse(char *str, int len);
-int intToStr(int x, char str[], int d);
+int intToStr(long long int x, char str[], int d);
 unsigned int un_len(unsigned int n);
 void width_full(s_info *ints, int flag);
 void prec_ful(s_info *ints);
@@ -77,21 +84,17 @@ void numb_if(int numb, s_info *ints);
 char *s_strcat(char *dest, const char *src);
 void unsint_if(unsigned int uns, s_info *ints);
 int check_help(s_info *ints, const char *format, int temp);
+long double make_round(long double num, int precision);
 size_t s21strlen(const char *str);
 
-int main() {
-  char *s21_test_str = (char *)malloc(sizeof(char) * 256);
-  char *lib_test_str = (char *)malloc(sizeof(char) * 256);
-  short i = 0;
-  short a = 32767;
-  int lib_res =
-      sprintf(lib_test_str, "|%hi| |%hd| |%hu|", i, a, (unsigned short)i);
-  int s21_res =
-      s21_sprintf(s21_test_str, "|%hi| |%hd| |%hu|", i, a, (unsigned short)i);
-  printf("%d\n", lib_res);
-  printf("%d\n", s21_res);
-  free(s21_test_str);
-  free(lib_test_str);
+// int main() {
+//   char data[100];
+//   char data1[100];
+//   s21_sprintf(data, "|%c %d|\n", '\0', 5);
+//   sprintf(data1, "|%c %d|\n", '\0', 5);
+//   printf("%s%s", data, data1);
+//   return 0;
+// }
 
-  return 0;
-}
+// пробел в "E" флаге, знак + и float
+
