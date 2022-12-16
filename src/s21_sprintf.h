af2441be0c9f74a2c5d2125972403434c3fbbb2a
marnitic <daniil.shelesteev@yandex.ru>
2022-12-16 17:36:00 +0300

    push make and test

--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -1,42 +1,39 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-// #include <string.h>
-#include <unistd.h>
+#include "s21_string.h"
 
-#define CHAR_PATTERN 4000
+#define CHAR_PATTERN 6000
 
 typedef long unsigned s21_size_t;
 
 typedef struct info {
-    va_list args;
-    int width;
-    int precision;
-    int hex_size;
-    int zero_padding;
-    int point;
-    int dash;
-    int total_length;
-    int sign;
-    int is_zero;
-    int space;
-    int hash;
-    float numb_fl;
-    int j_save_format;
-    int flag_f_float;
-    int flag_o;
-    int flag_e;
-    int flag_u;
-    int flag_h;
-    int flag_l;
-    int flag_s;
-    int flag_dash;
-    int flag_zero;
-    char full_buf[CHAR_PATTERN];
+  va_list args;
+  int width;
+  int precision;
+  int hex_size;
+  int zero_padding;
+  int point;
+  int dash;
+  int total_length;
+  int sign;
+  int is_zero;
+  int space;
+  int hash;
+  int j_save_format;
+  int flag_f_float;
+  int flag_o;
+  int flag_e;
+  int flag_u;
+  int flag_h;
+  int flag_l;
+  int flag_s;
+  int flag_dash;
+  int flag_zero;
+  int flag_float_zero;
+  int exp;
+  char full_buf[CHAR_PATTERN];
 } s_info;
 
-char *itoa(int value, char *result, int base);
-char *itoa_u(unsigned int value, char *result, int base);
+char *itoa(long long int value, char *result, int base);
+char *itoa_u(unsigned long long int value, char *result, int base);
 int s21_sprintf(char *str, const char *format, ...);
 void wparg_help(s_info *ints);
 char *s_strncat(char *dest, const char *src, s21_size_t n);
@@ -74,27 +71,36 @@ void scanndase(s_info *ints, unsigned long long numb);
 void point_b(s_info *ints, unsigned long long numb, char x);
 void put(int n, s_info *ints);
 void putnbr(unsigned n, s_info *ints);
-void litle_if(s_info *ints, int *numb);
-void else_if(s_info *ints, int *numb);
+void litle_if(s_info *ints, long long int *numb);
+void else_if(s_info *ints, long long int *numb);
 void scanndase_o(s_info *ints, unsigned long long numb);
 int str_else(char *s, s_info *ints);
 void hexup_help(s_info *ints, unsigned int hexup);
 void hex_help(s_info *ints, unsigned int hexal);
-void numb_if(int numb, s_info *ints);
+void numb_if(long long int numb, s_info *ints);
 char *s_strcat(char *dest, const char *src);
 void unsint_if(unsigned int uns, s_info *ints);
 int check_help(s_info *ints, const char *format, int temp);
-long double make_round(long double num, int precision);
 size_t s21strlen(const char *str);
 
+// + в F и в E
+// // пробел в F и не надо в  D
+// // доработать G
+
 // int main() {
-//   char data[100];
-//   char data1[100];
-//   s21_sprintf(data, "|%c %d|\n", '\0', 5);
-//   sprintf(data1, "|%c %d|\n", '\0', 5);
-//   printf("%s%s", data, data1);
+//   char s21_buffer[256] = {0};
+//   char buffer[256] = {0};
+//   int w = 0;
+//   int l = 0;
+//   sprintf(buffer, "%1.3g %01g%# *.*g %n", 10009000., 0.003, -10, 5,
+//           0.000074047471, &w);
+//   s21_sprintf(s21_buffer, "%1.3g %01g %# *.*g %n", 10009000., 0.003, -10,
+//               5, 0.000074047471, &l);
+//   printf("%s\n", s21_buffer);
+//   printf("%s\n", buffer);
+
+//     printf("%d\n", w);
+//   printf("%d\n", l);
+
 //   return 0;
 // }
-
-// пробел в "E" флаге, знак + и float
-
--- src/unit_test.h
+++ src/unit_test.h
@@ -1,31 +1,31 @@
-#ifndef SRC_TESTS_SUITCASES_H_
-#define SRC_TESTS_SUITCASES_H_
+#ifndef SRC_HEADERS_UNIT_TEST_H_
+#define SRC_HEADERS_UNIT_TEST_H_
 
 #include <check.h>
 #include <string.h>
 
 #include "s21_string.h"
 
-Suite *suite_strlen(void);
-Suite *suite_strspn(void);
-Suite *suite_strcspn(void);
-Suite *suite_memcmp(void);
-Suite *suite_strcmp(void);
-Suite *suite_strncmp(void);
-Suite *suite_memcpy(void);
-Suite *suite_memmove(void);
-Suite *suite_memset(void);
-Suite *suite_strcpy(void);
-Suite *suite_strncpy(void);
-Suite *suite_memchr(void);
-Suite *suite_strrchr(void);
-Suite *suite_strchr(void);
-Suite *suite_strpbrk(void);
-Suite *suite_strstr(void);
-Suite *suite_strtok(void);
-Suite *suite_strerror(void);
-Suite *suite_strcat(void);
-Suite *suite_strncat(void);
+//Suite *suite_strlen(void);
+//Suite *suite_strspn(void);
+//Suite *suite_strcspn(void);
+//Suite *suite_memcmp(void);
+//Suite *suite_strcmp(void);
+//Suite *suite_strncmp(void);
+//Suite *suite_memcpy(void);
+//Suite *suite_memmove(void);
+//Suite *suite_memset(void);
+//Suite *suite_strcpy(void);
+//Suite *suite_strncpy(void);
+//Suite *suite_memchr(void);
+//Suite *suite_strrchr(void);
+//Suite *suite_strchr(void);
+//Suite *suite_strpbrk(void);
+//Suite *suite_strstr(void);
+//Suite *suite_strtok(void);
+//Suite *suite_strerror(void);
+//Suite *suite_strcat(void);
+//Suite *suite_strncat(void);
 //Suite *suite_to_upper(void);
 //Suite *suite_to_lower(void);
 //Suite *suite_trim(void);
@@ -34,4 +34,4 @@ Suite *suite_sprintf(void);
 void run_tests(void);
 void run_testcase(Suite *testcase);
 
-#endif  // SRC_TESTS_SUITCASES_H_
+#endif  // SRC_HEADERS_UNIT_TEST_H_
